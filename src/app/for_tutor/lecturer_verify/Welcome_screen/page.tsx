62ee6a42e8bbc714abd3064ac032c73e1e4ba083
SALAMI TOYOSI <106937039+Toyostech@users.noreply.github.com>
2024-09-21 21:33:20 +0100

    first commit

---
+++ src/app/for_tutor/lecturer_verify/Welcome_screen/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='wrapper' style={{
+        padding: "15px 5vw"
+    }}>
+      
+    </div>
+  )
+}
+
+export default page
